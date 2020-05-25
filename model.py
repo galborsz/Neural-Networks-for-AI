eabde943eb9ab2551b362cd05062967fdb9d2f06
Gisela <galborsz@gmail.com>
2020-05-25 19:04:38 +0200

    select subset from data

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
 import numpy as numpy
 
-import keras
-from keras.layers import LSTM, Dense, Dropout
-from keras.models import Sequential, load_model
+#from keras.layers import LSTM, Dense, Dropout
+#from keras.models import Sequential, load_model
+import pandas as pd
+
+xls = pd.read_excel("/Users/giselaalbors/desktop/M3C.xls", index_col=0)
+selectedrows = xls.iloc[0:146]
+selectedcolumn = selectedrows.iloc[:, 5:25]
+data = selectedcolumn.values.tolist() #convert data into list
+print(data)
