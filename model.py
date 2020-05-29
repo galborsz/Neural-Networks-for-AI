d755409b49a8626cce2090e755a43d6e97ef4a54
Gisela <galborsz@gmail.com>
2020-05-29 08:49:07 +0200

    convert dataFrame into numpy array + comments

--- model.py
+++ model.py
@@ -1,23 +1,26 @@
 import os
 import sys
-import time
-import warnings
 import numpy as np
 import pandas as pd
-from numpy import newaxis
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 
 def load_data(filename):
 
+    #read data from csv file
     data = pd.read_csv(filename)
     df = pd.DataFrame(data) 
+
+    #select rows and columns
     df = df[:146]
     df.drop(df.columns[26:], axis=1, inplace=True)
     df.drop(df.columns[[0, 1, 2, 4, 5]], axis = 1, inplace = True) 
-    
 
+    #convert dataFrame into a numpy array
+    result = df.to_numpy()
+    
+    return result
 
 def main(argv):
     load_data("M3C.csv")
