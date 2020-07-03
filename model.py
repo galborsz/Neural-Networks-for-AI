6ee54429228542aaefc89243687348f0b8f39fbf
Gisela <galborsz@gmail.com>
2020-07-03 21:15:51 +0200

    adjusted polynomial to trend to find predicted values (by making the polynomial larger) + substracted these predicted values from the original predicted values to find the detrended version of the prediction

--- model.py
+++ model.py
@@ -86,13 +86,35 @@ def main(argv):
     # split between training and testing data
     test_dataset, training_dataset = train_test_split(raw_seq, train_size=0.8, test_size=0.2, random_state = 3)
 
-    #find trend and detrended version of the training data set
-    detrended, trend = detrending(training_dataset)
+    
 
     #split timeseries between input data and values to predict
     data, pred = split_sequence(training_dataset) 
-    data_detrended, pred_detrended = split_sequence(detrended) 
-    data_trend, pred_trend = split_sequence(trend) 
+
+    #find trend and detrended version of the training data set
+    detrended, trend = detrending(data)
+    print(len(trend[0]))
+    x = range(len(trend[1]))
+    trend_coeff = np.polyfit(list(x), trend[1], deg = 3)
+
+    
+    polyn = np.poly1d(trend_coeff)
+
+    x2 = range(len(trend[0]), 68)
+
+    """plt.plot(trend[1])
+    plt.plot(x, polyn(x))
+    plt.show()"""
+
+    detrended_pred = []
+    for serie in pred:
+        value = serie - polyn(x2)
+        detrended_pred.append(value)
+
+
+    #data_detrended, pred_detrended = split_sequence(detrended) 
+
+    #data_trend, pred_trend = split_sequence(trend) 
 
     # define model
     model = Sequential()
@@ -106,7 +128,7 @@ def main(argv):
     model.compile(optimizer='adam', loss='mse',metrics=['accuracy'])
 
     # fit model detrended data
-    model.fit(np.array(data_detrended), np.array(pred_detrended), batch_size=14 , epochs=30, verbose=1)
+    model.fit(np.array(detrended), np.array(detrended_pred), batch_size=14 , epochs=30, verbose=1)
 
     """# demonstrate prediction detrended data
     x_input_detrended = np.array(data_detrended[0])
@@ -123,7 +145,7 @@ def main(argv):
     plot(pred, yhat_detrended + yhat_trend)"""
 
     #evaluate the model with the training data
-    results = model.evaluate(np.array(data_detrended), np.array(pred_detrended))
+    results = model.evaluate(np.array(detrended), np.array(detrended_pred))
     print("test loss, test acc:", results)
 
     #find trend and detrended version of the testing data set
